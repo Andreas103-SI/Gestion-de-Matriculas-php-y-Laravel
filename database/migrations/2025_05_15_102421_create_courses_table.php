ebda7ec1752ec972a045dda41c332340e5ee1c0f
andreasierra <andreasierra1223@gmail.com>
2025-05-15 13:39:42 +0200

    Add authentication, email verification, and password management routes and tests
    - Created auth routes for registration, login, password reset, and email verification.
    - Implemented console command for displaying inspiring quotes.
    - Added web routes for dashboard and profile management.
    - Configured .gitignore files for various storage directories.
    - Set up Tailwind CSS configuration.
    - Developed feature tests for authentication, email verification, password confirmation, password reset, and profile management.
    - Established base test case and example tests for unit testing.
    - Configured Vite for asset management.

---
+++ database/migrations/2025_05_15_102421_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('specialty_code')->unique();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('active')->default(true);
+            $table->integer('theoretical_hours');
+            $table->integer('practical_hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
